c74a1c87d2a993765cc64c490bda4d7fe98da36b
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-20 10:31:19 -0800

    new

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,2 +1,2 @@
 cd ~/Code/MCB185/data
-gunzip -c dictionary.gz | grep "r" | grep "[oznrica]" | grep -v "[bdefghjklmpqstuvwxy]" | grep -E ".{4,}" | wc -l
+gunzip -c dictionary.gz | grep "r" | grep "[oznrica]" | grep -v "[bdefghjklmpqstuvwxy]" | grep -E ".{4,}" 
