9a3e52f30796f9640c0b696a5d28459172b1213e
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-23 09:14:44 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,12 @@
 Anisha Patel
 AnishaPatel77
+
+cd ~/Code/MCB185/data
+
+gunzip -c dictionary.gz | grep "a" | grep "[umocft]" | grep -v "[bdefghjklmpqstuvwxy]" | grep -E ".{4,}" 
+
+gunzip -c dictionary.gz | grep "b" | grep "[atirnl]" | grep -v "[bcdefghjkmopqsuvwxyz]" | grep -E ".{4,}" 
+
+gunzip -c dictionary.gz | grep "c" | grep "[amodin]" | grep -v "[bcefghjklmopqrstuvwxyz]" | grep -E ".{4,}" 
+
+gunzip -c dictionary.gz | grep "z" | grep "[naoigr]" | grep -v "[bcdefghjklmpqstuvwxyz]" | grep -E ".{4,}" 
