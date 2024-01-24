e04978883de5c4d8d1f3b69d9e49e07e5ee964ca
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-23 21:55:37 -0800

    new

--- 21quadratic.py
+++ 21quadratic.py
@@ -1,10 +1,10 @@
 # 21quadratic.py by Anisha Patel
 
 def quadratic(a, b, c):
-	root_1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-	root_2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
-	return root_1, root_2
+	x_1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+	x_2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+	return x_1, x_2
 
-print(quadratic(1, 2, 3))
-print(quadratic(3, 4, 5))
-print(quadratic(2, 4, 5))
+print("x-intercepts:", quadratic(1, 2, 3))
+print("x-intercepts:", quadratic(3, 4, 5))
+print("x-intercepts:", quadratic(2, 4, 5))
