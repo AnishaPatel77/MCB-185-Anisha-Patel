09a354b6762e3e0a87f6a123c02c74789700615d
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-23 21:58:54 -0800

    new

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -4,11 +4,11 @@ def oligo_temp(A, C, G, T):
 	oligo = (A + C + G + T) 
 	if oligo <= 13:
 		melting_temp = (A + T) * 2 + (G + C) * 4
-	if oligo > 13:
+	elif oligo > 13:
 		melting_temp = (64.9 + (41 * (G + C - 16.4))) / (A + T + G + C)
 	return melting_temp 
 	
-print(oligo_temp(1, 2, 3, 4), "degrees") # oligo length < 13
-print(oligo_temp(1, 3, 4, 5), "degrees") # oligo length = 13
-print(oligo_temp(3, 4, 5, 6), "degrees") # oligo length > 13
+print("Tm:", oligo_temp(1, 2, 3, 4), "degrees") # oligo length < 13
+print("Tm:", oligo_temp(1, 3, 4, 5), "degrees") # oligo length = 13
+print("Tm:", oligo_temp(3, 4, 5, 6), "degrees") # oligo length > 13
 
