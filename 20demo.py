8810a9b242d6c05008a829c5eabb8cddad15314d
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-23 21:52:22 -0800

    new

--- 20demo.py
+++ 20demo.py
@@ -1,3 +1,13 @@
 # 20demo.py by Anisha Patel
 
+import math 
+
 print('hello, again') # greeting
+print(1.5e-2)
+print(2 + 2)
+print(5 // 2)
+
+print(math.ceil(3))
+print(math.log10(2))
+print(math.pow(20, 10))
+
