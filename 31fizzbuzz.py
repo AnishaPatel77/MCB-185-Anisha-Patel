cf94ec5e2b2f448d1a7c002e27a872f46f83f440
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-25 15:44:59 -0800

    new

---
+++ 31fizzbuzz.py
@@ -0,0 +1,14 @@
+# 31fizzbuzz.py by Anisha Patel
+
+limit = 100
+for i in range(0, 100):
+	if i in range(0, 100, 5) and i in range(0, 100, 3):
+		print("FizzBuzz")
+	elif i in range(0, 100, 3):
+		print("Fizz")
+	elif i in range (0, 100, 5): 
+		print("Buzz")
+	else:
+		print(i)
+	
+	
