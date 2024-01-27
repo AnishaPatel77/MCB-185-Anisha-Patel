25db614f9aa1c2f236d0f58391a5b5cef69c3021
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-26 17:38:35 -0800

    new

--- 24accuracy.py
+++ 24accuracy.py
@@ -7,7 +7,7 @@ def data(tp, fp, tn, fn):
 	F1 = (2 * precision * recall) / (precision + recall)
 	return accuracy, F1
 	
-print("(accuracy,F1)", data(1, 2, 3, 4)) 
-print("(accuracy,F1)", data(2, 3, 4, 5)) 
-print("(accuracy,F1)", data(11213, 23243, 343241, 442341)) 
+print("(accuracy, F1)", data(1, 2, 3, 4)) 
+print("(accuracy, F1)", data(2, 3, 4, 5)) 
+print("(accuracy, F1)", data(11213, 23243, 343241, 442341)) 
  
