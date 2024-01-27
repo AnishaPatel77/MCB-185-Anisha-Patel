0ed94dde7e40fff120fa07744cfbd2b118f991ca
“AnishaPatel77” <anishapatel77@gmail.com>
2024-01-26 16:04:24 -0800

    new

--- 21quadratic.py
+++ 21quadratic.py
@@ -1,15 +1,15 @@
 # 21quadratic.py by Anisha Patel
 
 def quadratic(a, b, c):
-	discriminant = b**2 - 4 * a * c
+	discriminant = b ** 2 - 4 * a * c
 	if discriminant < 0:
 		return "no real solution"
 	if discriminant == 0:
-		x1 = (-b + (discriminant**0.5)) / 2 * a
+		x1 = (-b + (discriminant ** 0.5)) / 2 * a
 		return x1
 	if discriminant > 0:
-		x1 = (-b + (discriminant**0.5)) / 2 * a
-		x2 = (-b - (discriminant**0.5)) / 2 * a
+		x1 = (-b + (discriminant ** 0.5)) / 2 * a
+		x2 = (-b - (discriminant ** 0.5)) / 2 * a
 		return x1, x2
 
 print("x-intercept(s):", quadratic(-1, -2, -3)) # no real solution
