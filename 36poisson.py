3f33a325639cfd0eb3f5ee830c2f158e02e61ac6
AnishaPatel77 <localhome@campus-011-242.ucdavis.edu>
2024-02-01 10:02:04 -0800

    new

--- 36poisson.py
+++ 36poisson.py
@@ -1,1 +1,12 @@
 # 36poisson.py by Anisha Patel
+
+# Create a function that computes the Poisson probability of k events 
+# occuring with an expectation of n (n^k e^-n / k!) and demonstrate it works 
+# by calling it with several values of n and k.
+
+def factorial(n):
+	if n == 0: return 1
+	fac = 1
+	for i in range(1, n + 1):
+		fac = fac * i
+	return fac
