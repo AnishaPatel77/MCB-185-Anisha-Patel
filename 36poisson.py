363c58b3827760660046f644a5c49c5572633c93
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-01 17:19:56 -0800

    new

--- 36poisson.py
+++ 36poisson.py
@@ -5,8 +5,19 @@
 # by calling it with several values of n and k.
 
 def factorial(n):
-	if n == 0: return 1
+	if n == 0: 
+		return 1
 	fac = 1
 	for i in range(1, n + 1):
 		fac = fac * i
 	return fac
+	
+def poisson(n, k):
+	e = 2.7182818284590452353602874713527
+	prob = ((n ** k) * (e ** -n)) / factorial(k)
+	return prob
+	
+print(poisson(2, 1))
+print(poisson(4, 3))
+print(poisson(6, 5))
+print(poisson(0, 0))
