6e01cb989c0fa41557152fffb19368d238c89962
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-01 19:27:03 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,1 +1,14 @@
 # 34scoringmatrix.py by Anisha Patel
+
+seq = 'ACGT'
+print('  ', end=' ')
+for c in seq:
+	print(c, end='  ') 
+print()
+
+for nt1 in seq:
+	print(nt1, end=' ')
+	for nt2 in seq:
+		if nt1 == nt2: print('+1', end=' ')
+		else: print('-1', end=' ')
+	print()
