d85d17a941572337de00086c1a09e6fe0c59a6c9
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-02 15:32:53 -0800

    new

--- 35nchoosek.py
+++ 35nchoosek.py
@@ -11,13 +11,13 @@ def factorial(n):
 def nchoosek(n, k):
 	if n == 0 and k == 0: 
 		return 1
-	if k > n:
+	if n <=k:
 		return 0
 	if n > 0 and k > 0: 
 		return factorial(n) / (factorial(k) * factorial(n - k))		
 
 	
-print(nchoosek(2, 1))
+print(nchoosek(4, 2))
 print(nchoosek(4, 3))
 print(nchoosek(6, 5))
 print(nchoosek(0, 0))
