442a0a4f68f46d29101a06219c3856cf91cc817c
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-08 14:58:12 -0800

    new

--- 44randompi.py
+++ 44randompi.py
@@ -1,2 +1,20 @@
-# 44randompi.py by Anisha Patel
+# 44randompi.py by Anisha Patel & Varsha Thalladi
+
+import random
+import math
+
+inside = 0 
+total = 0 
+
+while True:
+	x = random.random()
+	y = random.random()
+	distance = math.sqrt(x**2 + y**2)
+	
+	if distance <= 1:
+		inside += 1
+	total += 1
+	
+	if total % 100000 == 0:
+		print(total, 4 * inside / total)
 
