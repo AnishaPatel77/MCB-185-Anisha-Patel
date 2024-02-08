d52db6125aeef47d9b232eba9ca362c291073b1b
“AnishaPatel77” <AnishaPatel@aces-MacBook-Pro.local>
2024-02-07 17:02:07 -0800

    new

--- 43randomdna.py
+++ 43randomdna.py
@@ -1,2 +1,10 @@
 # 43randomdna.py by Anisha Patel
 
+import random 
+
+def fasta_seq(num_seq, output_range):
+	for i in range(output_range):
+    	letters = (random.choice('ACGT'), end='')
+    	
+	return 
+ 
