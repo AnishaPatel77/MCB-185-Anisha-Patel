2d7e0dcd89cc6b7d7f6600ff519f9e7e864bb7f2
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-08 14:39:48 -0800

    new

--- 45dndstats.py
+++ 45dndstats.py
@@ -1,22 +1,55 @@
-# 45dndstats.py by Anisha Patel
+# 45dndstats.py by Anisha Patel & Varsha Thalladi
 
 import random 
 
-i = 0
-def 3d6(): # roll 3 six-sided dice
-	3d6_rslt = sum(random.randint(1, 6) for i in range(3))
-	return roll_3d6_rslt
+limit = 10 
 
-def 3d6r1(): # roll 3 six-sided dice, but re-roll any 1s
-	3d6r1_rslt = (random.randint(1, 6) for i in range(3))
-	for 1 in 3d6r1_rslt
-		3d6r1_rslt2 = sum(random.randint(1, 6) for i in range(3))
+# roll 3D6
+sum = 0 
+for i in range(limit):
+	for j in range(3):
+		dice = random.randint(1, 6)
+		sum += dice
+print (sum / limit)
 
-def 3D6x2(): # roll pairs of six-sided 3 times, taking the maximum each time
-	3D6x2_rslt = 
+# roll 3D6r1
+sum1 = 0
+for i in range(limit): 
+	for j in range(3):
+		dice_r1 = random.randint (1, 6)
+		if dice_r1 == 1:
+			dice_r1 == random.randint (1, 6) 
+		sum1 += dice_r1
+print (sum1 / limit)
 
-def 4D6d1(): # roll 4 six-sided dice, dropping the lowest die roll
-	4D6d1_rslt = 
+# roll 3D6×2
+sum2 = 0
+for i in range(limit): 
+	for j in range(3):
+		d1 = random.randint(1, 6)
+		d2 = random.randint (1, 6)
+		if d1 >= d2: sum2 += d1
+		else: sum2 += d2
+print (sum2 / limit)
 
-
+# roll 4D6d1
+sum3 = 0
+for i in range (4):
+	for j in range(4):
+		d1 = random.randint (1, 6)
+		d2 = random.randint (1, 6)
+		d3 = random.randint (1, 6)
+		d4 = random.randint (1, 6)
+		
+		min_roll = d1
+		if d2 < min_roll: min_roll = d2
+		if d3 < min_roll: min_roll = d3
+		if d4 < min_roll: min_roll = d4
 	
+		total = 0
+		if d1 != min_roll: total += d1
+		if d2 != min_roll: total += d2 
+		if d3 != min_roll: total += d3 
+		if d4 != min_roll: total += d4 
+		sum3 += total
+print (sum3 / limit)
