dd2f8ed1d50eab37b7d02b434bd628bf7e63fffd
“AnishaPatel77” <AnishaPatel@aces-MacBook-Pro.local>
2024-02-07 17:02:19 -0800

    new

--- 45dndstats.py
+++ 45dndstats.py
@@ -1,1 +1,22 @@
 # 45dndstats.py by Anisha Patel
+
+import random 
+
+i = 0
+def 3d6(): # roll 3 six-sided dice
+	3d6_rslt = sum(random.randint(1, 6) for i in range(3))
+	return roll_3d6_rslt
+
+def 3d6r1(): # roll 3 six-sided dice, but re-roll any 1s
+	3d6r1_rslt = (random.randint(1, 6) for i in range(3))
+	for 1 in 3d6r1_rslt
+		3d6r1_rslt2 = sum(random.randint(1, 6) for i in range(3))
+
+def 3D6x2(): # roll pairs of six-sided 3 times, taking the maximum each time
+	3D6x2_rslt = 
+
+def 4D6d1(): # roll 4 six-sided dice, dropping the lowest die roll
+	4D6d1_rslt = 
+
+
+	
