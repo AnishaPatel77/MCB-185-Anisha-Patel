b327134e51f53d765416378f2ec6878a587ec5dc
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-08 15:12:34 -0800

    new

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -1,1 +1,38 @@
-# 47deathsaves.py by Anisha Patel
+# 47deathsaves.py by Anisha Patel & Varsha Thalladi
+
+import random 
+
+limit = 100000
+
+revive = 0 
+died = 0
+stable = 0 
+
+for i in range(limit):
+	success = 0 
+	failure = 0 
+	
+	while True:
+		roll = random.randint(1, 20)
+		if roll == 20:
+			revive += 1
+			break
+		elif roll == 1:
+			failure += 2 
+			if failure >= 3:
+				died += 1
+				break
+		elif roll < 10:
+			failure += 1
+			if failure >+ 3:
+				died += 3
+				break
+		else:
+			success += 1
+			if success >= 3:
+				stable += 1
+				break
+
+print(f'revived: {revive / limit}')
+print(f'died: {died / limit}')
+print(f'stabilized: {stable / limit}')
