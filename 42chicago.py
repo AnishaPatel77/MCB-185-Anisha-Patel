4c26bb2b56ebd24da2bbd3d0d4ec3b2c20cec798
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-08 14:48:20 -0800

    new

--- 42chicago.py
+++ 42chicago.py
@@ -1,4 +1,7 @@
+# 42chicago.py by Anisha Patel
+
 import random 
+import sys 
 
 games = 10
 for i in range(games):
@@ -17,3 +20,18 @@ for i in range(games):
 			score += target
 	print(score) # final game score
 	
+
+games = 1000000 # 1 million trials
+log = games // 100 # report progress at 1% intervals
+total = 0
+zeroes = 0
+for i in range(games):
+	if i % log == 0: print(f'{100 * i/games:.0f}', file=sys.stderr)
+	score = 0
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target:
+			score += target
+	if score == 0: zeroes += 1
+	total += score
+print(total / games)
+print(zeroes / games)
