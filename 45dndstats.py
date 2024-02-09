686324c74708ce7de21391ba6a6bda4267c69722
AnishaPatel77 <localhome@MacBook-Pro-198.local>
2024-02-08 18:43:25 -0800

    new

--- 45dndstats.py
+++ 45dndstats.py
@@ -4,15 +4,15 @@ import random
 
 limit = 10 
 
-# roll 3D6
-sum = 0 
+# roll 3D6: roll 3 six-sided dice
+sum0 = 0 
 for i in range(limit):
 	for j in range(3):
 		dice = random.randint(1, 6)
-		sum += dice
-print(sum / limit)
+		sum0 += dice
+print(sum0 / limit)
 
-# roll 3D6r1
+# roll 3D6r1: roll 3 six-sided dice, but re-roll any 1s
 sum1 = 0
 for i in range(limit): 
 	for j in range(3):
@@ -22,7 +22,7 @@ for i in range(limit):
 		sum1 += dice_r1
 print(sum1 / limit)
 
-# roll 3D6×2
+# roll 3D6x2: roll pairs of six-sided 3 times, taking the maximum each time
 sum2 = 0
 for i in range(limit): 
 	for j in range(3):
@@ -32,7 +32,7 @@ for i in range(limit):
 		else: sum2 += d2
 print(sum2 / limit)
 
-# roll 4D6d1
+# roll 4D6d1: roll 4 six-sided dice, dropping the lowest die roll
 sum3 = 0
 for i in range(4):
 	for j in range(4):
