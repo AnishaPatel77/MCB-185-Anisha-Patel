ee710796db559f0de6a088d947fcd0e9e1333fe0
“AnishaPatel77” <anishapatel@Anishas-MacBook-Pro-2.local>
2024-02-29 16:36:05 -0800

    new

--- 64profinder.py
+++ 64profinder.py
@@ -1,6 +1,6 @@
 # 64profinder.py by Anisha Patel, Varsha, & Avantika
 
-# python3 64profinder.py ../MCB185/data/A.thaliana.fa.gz 100
+# python3 64profinder.py ecoli.fna.gz 100
 
 import sys
 import mcb185
@@ -9,40 +9,43 @@ import dogma
 fas = sys.argv[1]
 min_len = int(sys.argv[2])
 
-def six_frame_translation(dna):
+def find_proteins(trans_seq, min_len):
+	valid_proteins = []
+	protein = ''
+	start_found = False
+
+	for aa in trans_seq:
+		if aa == 'M' and not start_found:
+			start_found = True
+			protein += aa
+		elif start_found:
+			protein += aa
+			if aa == '*':
+				if len(protein) >= min_len:
+					valid_proteins.append(protein)
+				protein = ''
+				start_found = False
+	return valid_proteins
+
+def six_frame_translation(dna, min_len):
 	proteins = []
+	frames = [dna, dogma.revcomp(dna)]  
 
-	for frame in range(3):
-		protein = ''
-		for i in range(frame, len(dna) - 2, 3):
-			codon = dna[i:i+3]
-			protein += dogma.translate(codon)
-		proteins.append(protein)
-
-	rev_dna = dogma.revcomp(dna)
-	
-	for frame in range(3):
-		protein = ''
-		for i in range(frame, len(rev_dna) - 2, 3):
-			codon = rev_dna[i:i+3]
-			protein += dogma.translate(codon)
-		proteins.append(protein)
+	for dna_seq in frames:
+		for frame in range(3):
+			trans_seq = dogma.translate(dna_seq[frame:])
+			proteins += find_proteins(trans_seq, min_len)
 	return proteins
 
 output_proteins = []
-
 for name, seq in mcb185.read_fasta(fas):
-	
-	proteins = six_frame_translation(seq)
-
-	for i in range(len(proteins)):
-		protein = proteins[i]
-		if len(protein) >= min_len and protein[0] == 'M' and protein[-1] == '*':
-			identifier = f'{name}-prot-{i}'
-			output_proteins.append((identifier, protein))
+	proteins = six_frame_translation(seq, min_len)
+	for i, protein in enumerate(proteins):
+		identifier = f'{name}-prot-{i}'
+		output_proteins.append((identifier, protein))
 
 for identifier, protein in output_proteins:
-    print(f'>{identifier}')
-    for i in range(0, len(protein), 60):
-        print(protein[i:i+60])
+	print(f'>{identifier}')
+	for i in range(0, len(protein), 60):
+		print(protein[i:i+60])
 
