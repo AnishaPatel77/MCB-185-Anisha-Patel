7a82c89603430c233ce8938590ffe453e00f14e6
“AnishaPatel77” <anishapatel@Anishas-MacBook-Pro-2.local>
2024-03-18 16:58:47 -0700

    new

--- 81prosite.py
+++ 81prosite.py
@@ -5,16 +5,16 @@ import mcb185
 
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	if 'DKTGT' in seq: print(defline)
-    
+
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	if re.search('DKTGT', seq): print(defline)
-    
+
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	if re.search('DKTGT[LIVM][TI]', seq): print(defline)
-    
+
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	if re.search('C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H', seq): print(defline)
-    
+
 pat = '(C.{2,4}C.{3}[LIVMFYWC].{8}H.{3,5}H)'
 for defline, seq in mcb185.read_fasta(sys.argv[1]):
 	m = re.search(pat, seq)
