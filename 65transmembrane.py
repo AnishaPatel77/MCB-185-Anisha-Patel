dfb958c1066d8398b017d8f064d119a3264c0ecc
“AnishaPatel77” <anishapatel@Anishas-MacBook-Pro-2.local>
2024-03-18 17:03:06 -0700

    new

--- 65transmembrane.py
+++ 65transmembrane.py
@@ -6,25 +6,27 @@ import sys
 import mcb185
 import dogma
 
-for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	success = False
-	for i in range(0, 30 -8 + 1):
-		signalpeptide = seq[i:i+8]
-		signalpeptideavg = 0
-		for i in signalpeptide:
-			signalpeptideavg += dogma.kyte_doolittle(i)
-		signalpeptideavg = signalpeptideavg / 8
-		if signalpeptideavg >= 2.5 and 'P' not in signalpeptide:
-			for j in range(31, len(seq) - 11 + 1):
-				targetregion = seq[j:j+11]
-				targetregionavg = 0
-				for k in targetregion:
-					targetregionavg += dogma.kyte_doolittle(k)
-				targetregionavg = targetregionavg / 11
-				if targetregionavg >= 2.0 and 'P' not in targetregion:
-					success = True
-					#print(defline)
-					break
-		if success == True:
-			print(defline)
-			break
+def transmembrane(file):  
+	for defline, seq in mcb185.read_fasta(file):
+		success = False
+		for i in range(0, 30 -8 + 1):
+			signal_peptide = seq[i:i+8]
+			signal_peptide_avg = 0
+			for i in signal_peptide:
+				signal_peptide_avg += dogma.kyte_doolittle(i)
+			signal_peptide_avg = signal_peptide_avg / 8
+			if signal_peptide_avg >= 2.5 and 'P' not in signal_peptide:
+				for j in range(31, len(seq) - 11 + 1):
+					target_region = seq[j:j+11]
+					target_region_avg = 0
+					for k in target_region:
+						target_region_avg += dogma.kyte_doolittle(k)
+					target_region_avg = target_region_avg / 11
+					if target_region_avg >= 2.0 and 'P' not in target_region:
+						success = True
+						break
+			if success == True:
+				print(defline)
+				break
+
+transmembrane(sys.argv[1])  
